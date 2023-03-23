f90224a47adb8411e2209f46e629820d8ff7afb0
Kamil <95419657+Frqs69@users.noreply.github.com>
2023-03-23 21:18:27 +0100

    error handling

--- 4-natours/starter/app.js
+++ 4-natours/starter/app.js
@@ -4,7 +4,7 @@ const userRouter = require('./routes/userRoutes');
 const tourRouter = require('./routes/tourRoutes');
 const app = express();
 
-// -------------- MIDLEWARES -----------------------------
+// -------------- MIDDLEWARES -----------------------------
 // if we are on dev mode use morgan
 // console.log(process.env.NODE_ENV)
 if (process.env.NODE_ENV === 'development') {
@@ -18,10 +18,10 @@ app.use(express.json());
 app.use(express.static(`${__dirname}/public`));
 
 // create own middleware
-app.use((req, res, next) => {
-  console.log('Hello from middleware');
-  next();
-});
+// app.use((req, res, next) => {
+//   console.log('Hello from middleware');
+//   next();
+// });
 
 app.use((req, res, next) => {
   req.requestTime = new Date().toISOString();
@@ -49,5 +49,30 @@ app.use((req, res, next) => {
 // mounting new router on a route
 app.use('/api/v1/tours', tourRouter);
 app.use('/api/v1/users', userRouter);
+
+//handle other routes which not exist - need to be last route
+app.all('*', (req, res, next) => {
+  // res.status(404).json({
+  //   status: 'fail',
+  //   message: `Can't find ${req.originalUrl} on this server`,
+  // });
+
+  const err = new Error(`Can't find ${req.originalUrl} on this server`);
+  err.status = 'fail';
+  err.statusCode = 404;
+
+  next(err);
+});
+
+//create global error handling middleware
+app.use((err, req, res, next) => {
+  err.statusCode = err.statusCode || 500;
+  err.status = err.status || 'error';
+
+  res.status(err.statusCode).json({
+    status: err.status,
+    message: err.message,
+  });
+});
 
 module.exports = app;
