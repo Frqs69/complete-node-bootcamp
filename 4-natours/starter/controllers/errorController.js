e29117abc3ad50041a563056bee92ee5a6203c71
Kamil <95419657+Frqs69@users.noreply.github.com>
2023-03-25 20:32:58 +0100

    Next error handling lesson

--- 4-natours/starter/controllers/errorController.js
+++ 4-natours/starter/controllers/errorController.js
@@ -1,3 +1,23 @@
+const AppError = require('./../utils/appError')
+
+const handleCastErrorDB = (err) => {
+  const message = `Invalid ${err.path}: ${err.value}`;
+  return new AppError(message, 400)
+}
+
+const handleDuplicateFieldsDB = (err) => {
+  const value = err.errmsg.match(/(["'])(\\?.)*?\1/)[0]
+  const message = `Duplicate field value: ${value}. Please use another value!`;
+  return new AppError(message, 400)
+}
+
+const handleValidationErrorDB = err =>{
+  const errors = Object.values(err.errors).map(el => el.message)
+
+  const message = `Invalid input data. ${errors.join('. ')}`
+  return new AppError(message,400)
+}
+
 const sendErrorDev = (err, res) => {
   res.status(err.statusCode).json({
     status: err.status,
@@ -35,6 +55,18 @@ module.exports = (err, req, res, next) => {
   if (process.env.NODE_ENV === 'development') {
     sendErrorDev(err, res);
   } else if (process.env.NODE_ENV === 'production') {
-    sendErrorProd(err, res);
+    // create copy of err object
+    let error = {...err}
+
+    //  error is when creating, updating or getting data from database which not exist
+    if(error.name === 'CastError') error = handleCastErrorDB(error)
+
+    // error of duplicate fields
+    if(error.code === 11000) error = handleDuplicateFieldsDB(error)
+
+    //validation error
+    if(error.name === 'ValidationError') error = handleValidationErrorDB(error)
+
+    sendErrorProd(error, res);
   }
 };
--- 4-natours/starter/server.js
+++ 4-natours/starter/server.js
@@ -1,6 +1,13 @@
 const dotenv = require('dotenv');
 const mongoose = require('mongoose');
 
+// caught error outside express - exceptions
+process.on('uncaughtException', (err) => {
+  console.log('Unhandled exception. Shutting down...');
+  console.log(err.name, err.message);
+  process.exit(1);
+});
+
 // environment variables - Express
 // console.log(app.get('env'));
 
@@ -26,6 +33,15 @@ const app = require('./app');
 
 // create server on port 3000 --------------------------------
 const port = process.env.PORT || 8000;
-app.listen(port, () => {
+const server = app.listen(port, () => {
   console.log(`App running on port ${port}`);
+});
+
+// caught error outside express - rejections
+process.on('unhandledRejection', (err) => {
+  console.log('Unhandled rejection. Shutting down...');
+  console.log(err.name, err.message);
+  server.close(() => {
+    process.exit(1);
+  });
 });
